2426cb786e4534da687b9467390ca51cb3cc2399
Andy Damesón <andresdameson@gmail.com>
2021-06-05 17:00:00 -0300

    Agrega página de gracias para contacto

---
+++ pages/thank-you-page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <InternaTema>Información Útil</InternaTema>
+    <h1>Consultas</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Consultas - Gracias',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
